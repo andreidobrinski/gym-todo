2f802100478f779434a85b1795898e6fac1b1edf
Andrei Dobrinski <andreiidobrinski@gmail.com>
2020-04-06 14:53:54 -0400

    add footer

--- src/App.tsx
+++ src/App.tsx
@@ -7,13 +7,22 @@ import { Logo } from './assets/Logo';
 export const App = () => {
   return (
     <Outer>
-      <Header>
-        <Logo size={64} />
-        <h1>Gym Todo</h1>
-      </Header>
       <Inner>
-        <TodoList />
-        <AddItem />
+        <Header>
+          <Logo size={64} />
+          <h1>Gym Todo</h1>
+        </Header>
+        <Main>
+          <TodoList />
+          <AddItem />
+        </Main>
+        <Footer>
+          <a href="https://github.com/andreidobrinski/gym-todo">Made</a> with{' '}
+          <span role="img" aria-label="love">
+            &#10084;&#65039;
+          </span>{' '}
+          by <a href="https://andreidobrinski.com">Andrei Dobrinski</a>
+        </Footer>
       </Inner>
     </Outer>
   );
@@ -21,16 +30,25 @@ export const App = () => {
 
 const Outer = styled.div`
   background-color: var(--bg);
-  height: 100vh;
+  min-height: 100vh;
   width: 100vw;
   padding: 16px;
 `;
 
-const Inner = styled.main`
+const Inner = styled.div`
   display: flex;
   flex-direction: column;
   max-width: 600px;
-  margin: auto;
+  margin: 0 auto;
+  height: 100%;
+  min-height: calc(100vh - 32px);
+`;
+
+const Main = styled.main`
+  display: flex;
+  flex-direction: column;
+  margin: 0 auto;
+  width: 100%;
   padding: 16px;
   border-radius: 8px;
   background-color: var(--light-bg);
@@ -39,9 +57,14 @@ const Inner = styled.main`
 const Header = styled.header`
   display: flex;
   align-items: center;
-  margin: auto auto 16px auto;
-  max-width: 600px;
+  margin: 0 auto 16px 0;
   h1 {
     margin: 0 0 0 16px;
   }
+`;
+
+const Footer = styled.footer`
+  max-width: 600px;
+  margin: auto 0 0 auto;
+  padding-top: 32px;
 `;
